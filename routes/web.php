4008da77b1112806be064e01fae6ba72adc75a58
Ravi <rapianur26@gmail.com>
2026-09-21 23:58:17 +0700

    Selesai tugas CRUD helpdesk dan matriks kasus uji

---
+++ routes/web.php
@@ -0,0 +1,19 @@
+<?php
+
+use App\Http\Controllers\TicketController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/tickets', [TicketController::class, 'index'])
+    ->name('tickets.index');
+
+Route::get('/tickets/{ticket}', [TicketController::class, 'show'])
+    ->whereNumber('ticket')
+    ->name('tickets.show');
+
+Route::get('/api/tickets/{ticket}', [TicketController::class, 'showJson'])
+    ->whereNumber('ticket')
+    ->name('tickets.show-json');
+
+Route::pattern('ticket', '[0-9]+');
+
+Route::resource('tickets', TicketController::class);
